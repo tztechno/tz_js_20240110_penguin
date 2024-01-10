7810be4a300ff278ba571590b2537d1576bf7de2
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-10 12:02:28 +0900

    Add files via upload

---
+++ show_csv.js
@@ -0,0 +1,38 @@
+// CSVデータを取得する関数
+function fetchCSV(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            callback(xhr.responseText);
+        }
+    };
+    xhr.open("GET", url, true);
+    xhr.send();
+}
+
+// CSVデータをHTMLテーブルに変換する関数
+function convertCSVtoHTML(csvData) {
+    var rows = csvData.split('\n');
+    var tableHTML = '<table>';
+    
+    rows.forEach(function(row) {
+        tableHTML += '<tr>';
+        var columns = row.split(',');
+        columns.forEach(function(column) {
+            tableHTML += '<td>' + column + '</td>';
+        });
+        tableHTML += '</tr>';
+    });
+
+    tableHTML += '</table>';
+    return tableHTML;
+}
+
+// CSVファイルのURL
+var csvUrl = 'ishikawa_202401.csv';
+
+// CSVデータを取得してHTMLに変換し、テーブルコンテナに挿入する
+fetchCSV(csvUrl, function(csvData) {
+    var htmlTable = convertCSVtoHTML(csvData);
+    document.getElementById('table-container').innerHTML = htmlTable;
+});
